52ac833983d309d57f8cd188205560ff2d758d91
Sapna <73276625+sapnak23@users.noreply.github.com>
2025-06-24 17:42:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,50 @@ resource "azurerm_subnet" "private_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+# Create a Network Security Group (NSG)
+resource "azurerm_network_security_group" "nsg_web" {
+  name                = "nsg-web"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Terraform-HA"
+  }
+}
+
+# Terraform Code to Associate NSG to Subnets
+resource "azurerm_subnet_network_security_group_association" "nsg_public" {
+  subnet_id                 = azurerm_subnet.public_subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg_web.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_private" {
+  subnet_id                 = azurerm_subnet.private_subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg_web.id
+}
+
