4a7baa1639b5f1cd584a897bb07c729180a31a81
Sapna <73276625+sapnak23@users.noreply.github.com>
2025-06-24 17:03:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,33 @@
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-ha-infra"
   location = var.location
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-ha-infra"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    environment = "demo"
+  }
+}
+
+# Public Subnet
+resource "azurerm_subnet" "public_subnet" {
+  name                 = "public-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Private Subnet
+resource "azurerm_subnet" "private_subnet" {
+  name                 = "private-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
