1e85f7643dfbe915cc1054d8aa596b0074dfa49f
Sapna <73276625+sapnak23@users.noreply.github.com>
2025-06-24 11:30:07 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features = {}
+  subscription_id = "1c7a8aab-b547-40d2-a688-c168d850d8c8"
+}
