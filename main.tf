7259f14f4d1397d587465f2e7db671bb5fdb4556
Sapna <73276625+sapnak23@users.noreply.github.com>
2025-06-25 11:26:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,71 @@
-# Resource Group
+# Create a Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-ha-infra"
   location = var.location
 }
 
-# Virtual Network
+# Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-ha-infra"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  tags = {
-    environment = "demo"
-  }
 }
 
-# Public Subnet
-resource "azurerm_subnet" "public_subnet" {
-  name                 = "public-subnet"
+# Public Subnet 1
+resource "azurerm_subnet" "public_subnet1" {
+  name                 = "public-subnet-1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Private Subnet
-resource "azurerm_subnet" "private_subnet" {
-  name                 = "private-subnet"
+# Private Subnet 1
+resource "azurerm_subnet" "private_subnet1" {
+  name                 = "private-subnet-1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Create a Network Security Group (NSG)
+# Public IP for NAT Gateway
+resource "azurerm_public_ip" "nat_ip" {
+  name                = "nat-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# NAT Gateway for outbound internet on private subnet
+resource "azurerm_nat_gateway" "nat_gw" {
+  name                = "nat-gateway"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Standard"
+  idle_timeout_in_minutes = 10
+}
+
+# Attach Public IP to NAT Gateway
+resource "azurerm_nat_gateway_public_ip_association" "nat_ip_assoc" {
+  nat_gateway_id       = azurerm_nat_gateway.nat_gw.id
+  public_ip_address_id = azurerm_public_ip.nat_ip.id
+}
+
+# Associate NAT Gateway with Private Subnet
+resource "azurerm_subnet_nat_gateway_association" "private_nat_assoc" {
+  subnet_id      = azurerm_subnet.private_subnet1.id
+  nat_gateway_id = azurerm_nat_gateway.nat_gw.id
+}
+
+# Network Security Group for Public Subnet
 resource "azurerm_network_security_group" "nsg_web" {
   name                = "nsg-web"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "Allow-HTTP"
+    name                       = "AllowHTTP"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
@@ -51,7 +77,7 @@ resource "azurerm_network_security_group" "nsg_web" {
   }
 
   security_rule {
-    name                       = "Allow-SSH"
+    name                       = "AllowSSH"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
@@ -61,20 +87,11 @@ resource "azurerm_network_security_group" "nsg_web" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-  tags = {
-    environment = "Terraform-HA"
-  }
 }
 
-# Terraform Code to Associate NSG to Subnets
+# Associate NSG with Public Subnet
 resource "azurerm_subnet_network_security_group_association" "nsg_public" {
   subnet_id                 = azurerm_subnet.public_subnet1.id
-  network_security_group_id = azurerm_network_security_group.nsg_web.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "nsg_private" {
-  subnet_id                 = azurerm_subnet.private_subnet1.id
   network_security_group_id = azurerm_network_security_group.nsg_web.id
 }
 
