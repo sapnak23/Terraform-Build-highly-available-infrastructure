031c802be9d1ed611671451f1ba7e42145cd0a36
Sapna <73276625+sapnak23@users.noreply.github.com>
2025-06-26 12:04:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 # Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-ha-infra"
-  address_space       = ["10.0.0.0/16"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
@@ -39,11 +39,11 @@ resource "azurerm_public_ip" "nat_ip" {
 
 # NAT Gateway for outbound internet on private subnet
 resource "azurerm_nat_gateway" "nat_gw" {
-  name                = "nat-gateway"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Standard"
-  idle_timeout_in_minutes = 10
+  name                     = "nat-gateway"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  sku_name                 = "Standard"
+  idle_timeout_in_minutes  = 10
 }
 
 # Attach Public IP to NAT Gateway
@@ -94,4 +94,188 @@ resource "azurerm_subnet_network_security_group_association" "nsg_public" {
   subnet_id                 = azurerm_subnet.public_subnet1.id
   network_security_group_id = azurerm_network_security_group.nsg_web.id
 }
+
+# Create SSH Key
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Public VM
+resource "azurerm_linux_virtual_machine" "public_vm" {
+  name                  = "vm-public"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.public_nic.id]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+ source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo systemctl enable apache2
+sudo systemctl start apache2
+EOF
+  )
+
+  tags = {
+    environment = "public"
+  }
+}
+
+# NIC for Public VM
+resource "azurerm_network_interface" "public_nic" {
+  name                = "public-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.public_subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+}
+
+# Public IP for VM
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "public-ip-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+# Create Network Interface for Private VM (no public IP)
+resource "azurerm_network_interface" "private_nic" {
+  name                = "private-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.private_subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Create Linux VM in Private Subnet
+resource "azurerm_linux_virtual_machine" "private_vm" {
+  name                = "vm-private"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.private_nic.id
+  ]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+ source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo systemctl enable apache2
+sudo systemctl start apache2
+EOF
+  )
+
+  tags = {
+    environment = "private"
+  }
+}
+
+# Public IP for Load Balancer
+resource "azurerm_public_ip" "lb_public_ip" {
+  name                = "lb-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+# Load Balancer
+resource "azurerm_lb" "app_lb" {
+  name                = "app-loadbalancer"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Basic"
+
+  frontend_ip_configuration {
+    name                 = "frontend"
+    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
+  }
+}
+
+# Backend Address Pool (no resource_group_name required)
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  name            = "backend-pool"
+  loadbalancer_id = azurerm_lb.app_lb.id
+}
+
+#  Health Probe (no resource_group_name required)
+resource "azurerm_lb_probe" "http_probe" {
+  name                = "http-probe"
+  loadbalancer_id     = azurerm_lb.app_lb.id
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+#  Load Balancing Rule
+resource "azurerm_lb_rule" "http_rule" {
+  name                           = "http-rule"
+  loadbalancer_id                = azurerm_lb.app_lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "frontend"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bepool.id]
+  probe_id                       = azurerm_lb_probe.http_probe.id
+}
+
+#  Associate Public VM with Load Balancer
+resource "azurerm_network_interface_backend_address_pool_association" "nic_lb_association" {
+  network_interface_id    = azurerm_network_interface.public_nic.id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bepool.id
+}
+
 
